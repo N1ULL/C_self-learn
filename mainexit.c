c8b261030777e56dc37f84336b1c7ce05a02a42e
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-04-22 18:04:39 +0800

    Add files via upload

---
+++ mainexit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+void fun()
+{
+    printf("123\n");
+    printf("123\n");
+    printf("123\n");
+    exit(0);
+    printf("123\n");
+    printf("123\n");
+    printf("123\n");
+}
+int main()
+{
+    fun();
+
+    return EXIT_SUCCESS;
+}
